8e1d82a1cf5cb504d52735dad69d4e34673a4b25
XiaoYuOvO <37735771+X1AOYu233@users.noreply.github.com>
2021-02-05 14:13:23 +0800

    test

--- src/net/xiaoyu233/fml/AbstractMod.java
+++ src/net/xiaoyu233/fml/AbstractMod.java
@@ -3,9 +3,12 @@ package net.xiaoyu233.fml;
 public abstract class AbstractMod {
     public AbstractMod(){}
     public abstract void preInit();
-    public void postInit(){};
+    public void postInit(){}
+
     public abstract String modId();
     public abstract int modVerNum();
     public abstract String modVerStr();
+
+
     public abstract String transformPkg();
 }
