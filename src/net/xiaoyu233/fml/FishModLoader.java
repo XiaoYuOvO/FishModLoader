aa70c76ec7634e2e388e46ce3ce661bf2af1963c
Xiao_Yu233 <37735771+XiaoYuOvO@users.noreply.github.com>
2024-03-01 21:45:26 +0800

    Update version

--- src/net/xiaoyu233/fml/FishModLoader.java
+++ src/net/xiaoyu233/fml/FishModLoader.java
@@ -36,8 +36,8 @@ public class FishModLoader extends AbstractMod{
    public static final File MOD_DIR = new File("mods");
    private static final Map<String, ModInfo> modsMapForLoginCheck;
    private static final boolean allowsClientMods;
-   public static final String VERSION = "v1.4.2";
-   public static final int VERSION_NUM = 142;
+   public static final String VERSION = "v1.5.0";
+   public static final int VERSION_NUM = 150;
    private static final ArrayList<ModInfo> mods = new ArrayList<>();
    private static final Map<String, ModInfo> modsMap = new HashMap<>();
    private static boolean isServer = false;
